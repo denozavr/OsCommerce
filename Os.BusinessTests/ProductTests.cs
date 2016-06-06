cf834bf18ea0ca92ac8f36a85f564a717f596cb9
Denis <denizdeniz90@gmail.com>
2016-06-06 16:54:57 +0300

    Add Unit Test for Product class

---
+++ Os.BusinessTests/ProductTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Os.BusinessLayer;
+
+namespace Os.BusinessTests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void PrintProductTest()
+        {
+            //arrange
+            var product = new Product() {ProductName = "Ladder", ProductId = 1, Description = "10 feet"};
+            var expected = "It's Ladder (1): 10 feet";
+            //act
+            var actual = product.PrintProduct();
+
+            //assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
+
