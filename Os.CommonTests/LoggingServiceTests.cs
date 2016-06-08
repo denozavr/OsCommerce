71df625f949a50a8358a5d8f1437a504bf7e877a
Denis <denizdeniz90@gmail.com>
2016-06-08 17:49:32 +0300

    Add test for Logging Service

---
+++ Os.CommonTests/LoggingServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Os.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Os.Common.Tests
+{
+    [TestClass()]
+    public class LoggingServiceTests
+    {
+        [TestMethod()]
+        public void LogActionTest()
+        {
+            //Arrange
+            var expected = "Action: Test";
+
+            //Act
+            var actual = LoggingService.LogAction("Test");
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
