6311a0f4dc5bb388adfa9642a3c875de73262a6c
Denis <denizdeniz90@gmail.com>
2016-06-08 17:43:26 +0300

    Add common library and simple static logging class

---
+++ Os.Common/LoggingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Os.Common
+{
+    /// <summary>
+    /// Providing logging.
+    /// </summary>
+    public static class LoggingService
+    {
+        /// <summary>
+        /// Logs actions.
+        /// </summary>
+        /// <param name="action">Action to log</param>
+        /// <returns></returns>
+        public static string LogAction(string action)
+        {
+            var logText = "Action: " + action;
+            Console.WriteLine(logText);
+
+            return logText;
+        }
+    }
+}
