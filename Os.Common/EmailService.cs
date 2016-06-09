8a7be588d654de782016a75af62228cf1b4e887d
Denis <denizdeniz90@gmail.com>
2016-06-09 09:03:11 +0300

    Add simple Email static Service

---
+++ Os.Common/EmailService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Os.Common
+{
+    public class EmailService
+    {
+        /// <summary>
+        /// Send email message.
+        /// </summary>
+        /// <param name="subject">Subject of the message.</param>
+        /// <param name="message">Message text.</param>
+        /// <param name="recipient">Email address of the message recepient.</param>
+        /// <returns></returns>
+        public string SendMessage(string subject, string message, string recipient)
+        {
+            var confirmation = "Message sent" + subject;
+            LoggingService.LogAction(confirmation);
+
+            return confirmation;
+        }
+    }
+}
